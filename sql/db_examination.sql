2e4b49f16172d6a42589b863dd2123de697ee407
thu <mingfei.net@gmail.com>
2016-05-19 09:42:45 +0800

    Update.

--- sql/db_examination.sql
+++ sql/db_examination.sql
@@ -4,46 +4,101 @@ CREATE DATABASE db_examination;
 -- table admin
 DROP TABLE IF EXISTS db_examination.admin;
 CREATE TABLE db_examination.admin (
-  id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email    VARCHAR(191) UNIQUE,
-  username VARCHAR(255),
-  password VARCHAR(255),
-  role     CHAR(1) COMMENT 's-system, t-teacher, a-administration'
-);
+  id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  email    VARCHAR(191) NOT NULL  UNIQUE
+  COMMENT '邮箱',
+  username VARCHAR(255) NOT NULL
+  COMMENT '姓名',
+  password VARCHAR(255) NOT NULL
+  COMMENT '密码'
+)
+  COMMENT '管理员表';
+
+-- table teacher
+DROP TABLE IF EXISTS db_examination.teacher;
+CREATE TABLE db_examination.teacher (
+  id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  email    VARCHAR(191) NOT NULL  UNIQUE
+  COMMENT '邮箱',
+  username VARCHAR(255) NOT NULL
+  COMMENT '姓名',
+  password VARCHAR(255) NOT NULL
+  COMMENT '密码'
+)
+  COMMENT '教师表';
+
+-- table assistant
+DROP TABLE IF EXISTS db_examination.assistant;
+CREATE TABLE db_examination.assistant (
+  id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  email    VARCHAR(191) NOT NULL  UNIQUE
+  COMMENT '邮箱',
+  username VARCHAR(255) NOT NULL
+  COMMENT '姓名',
+  password VARCHAR(255) NOT NULL
+  COMMENT '密码'
+)
+  COMMENT '教务表';
 
 INSERT INTO db_examination.admin
-VALUES (NULL, 'system@qq.com', 'system', '13do3DeGj6b8Nxf0l3+J/ER05/yQzbHHPkaIhb4m01f+p0nj14OrJEIts4K2qZ3m', 's');
-
--- table student
-DROP TABLE IF EXISTS db_examination.student;
-CREATE TABLE db_examination.student (
-  id         INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  email      VARCHAR(191) UNIQUE,
-  username   VARCHAR(255),
-  password   VARCHAR(255),
-  photo      VARCHAR(255),
-  last_ip    VARCHAR(255),
-  last_login DATETIME,
-  class_id   INT(11) UNSIGNED
-);
+VALUES (NULL, 'system@qq.com', 'system', '13do3DeGj6b8Nxf0l3+J/ER05/yQzbHHPkaIhb4m01f+p0nj14OrJEIts4K2qZ3m');
 
 -- table class
 DROP TABLE IF EXISTS db_examination.class;
 CREATE TABLE db_examination.class (
-  id          INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name        VARCHAR(255),
-  schedule    VARCHAR(255),
-  start_date  DATE,
-  finish_date DATE
-);
+  id         INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  name       VARCHAR(255) NOT NULL
+  COMMENT '班级名称',
+  startDate  DATE         NOT NULL
+  COMMENT '开班日期',
+  finishDate DATE         NOT NULL
+  COMMENT '结业日期'
+)
+  COMMENT '班级表';
+
+-- table student
+DROP TABLE IF EXISTS db_examination.student;
+CREATE TABLE db_examination.student (
+  id         INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  COMMENT 'PK',
+  email      VARCHAR(191) NOT NULL UNIQUE
+  COMMENT '邮箱',
+  username   VARCHAR(255) NOT NULL
+  COMMENT '姓名',
+  password   VARCHAR(255) NOT NULL
+  COMMENT '密码',
+  gender     CHAR(1)      NOT NULL
+  COMMENT '性别',
+  dob        DATE         NOT NULL
+  COMMENT '出生日期',
+  tel        VARCHAR(255) COMMENT '电话',
+  education  VARCHAR(255) COMMENT '学历',
+  major      VARCHAR(255) COMMENT '专业',
+  academy    VARCHAR(255) COMMENT '毕业院校',
+  graduation DATE COMMENT '毕业日期',
+  training   VARCHAR(255) COMMENT '培训经历',
+  experience VARCHAR(255) COMMENT '工作经验',
+  photo      VARCHAR(255) NOT NULL
+  COMMENT '照片',
+  remark     VARCHAR(255) COMMENT '备注',
+  lastIp     VARCHAR(255) NOT NULL
+  COMMENT '最后登录IP',
+  lastLogin  DATETIME     NOT NULL
+  COMMENT '最后登录时间',
+  classId    INT(11) UNSIGNED COMMENT 'FK 班级ID'
+)
+  COMMENT '学生表';
 
 
 ALTER TABLE db_examination.student
-ADD CONSTRAINT
-  fk_student_class_id
-FOREIGN KEY (class_id)
+  ADD CONSTRAINT
+  fk_student_classId
+FOREIGN KEY (classId)
 REFERENCES db_examination.class (id);
-
 
 SELECT *
 FROM db_examination.admin;
@@ -57,5 +112,8 @@ FROM db_examination.class;
 SELECT
   s.username,
   c.name
-FROM db_examination.student s INNER JOIN db_examination.class c
-    ON s.class_id = c.id;
+FROM db_examination.student s INNER JOIN db_examination.class c ON s.classId = c.id;
+
+SHOW TABLE STATUS FROM db_examination;
+
+SHOW FULL COLUMNS FROM db_examination.student;
