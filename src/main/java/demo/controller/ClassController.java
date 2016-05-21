43990fc8cff5d2abdd01b3df112c9fff241b3fb0
mingfei <mingfei.net@gmail.com>
2016-05-21 08:36:34 +0800

    Update.

--- src/main/java/demo/controller/ClassController.java
+++ src/main/java/demo/controller/ClassController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/classController")
 public class ClassController extends BaseController {
@@ -15,8 +17,10 @@ public class ClassController extends BaseController {
     private SqlSessionFactory sqlSessionFactory;
 
     @RequestMapping("/remove/{id}")
-    private void remove(@PathVariable int id) {
+    private void remove(@PathVariable int id) throws IOException {
         System.out.println("id: " + id);
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        sqlSession.delete("class.remove", id);
+        response.sendRedirect("/class/queryAll");
     }
 }
